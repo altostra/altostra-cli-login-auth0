622fce8c1e48d6df0f08723f0cc8f68c2955492d
Yev Krupetsky <yevk@altostra.com>
2020-05-06 10:57:40 +0300

    Refactored code styles, types, and cleanup
    Cleaned code style a bit, no more new lines after 'export' keyword.
    Removed any remaining semicolons.
    Changed 'type' definitions with 'interface' definitions wherever makes
    sense.

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,19 @@
 import crypto from 'crypto'
 
-export
-function isNonEmptyString(s: unknown): s is string {
+export function isNonEmptyString(s: unknown): s is string {
   return typeof s === 'string' && s.length > 0
 }
 
-export 
-type Deferred<T> = {
+export interface Deferred<T> {
   resolve: (value?: T) => void
   reject: (error?: unknown) => void
   promise: Promise<T>
 }
 
-export 
-function mkDeferred<T>(): Deferred<T> {
+export function mkDeferred<T>(): Deferred<T> {
   let resolvePromise: (value?: T) => void
   let rejectPromise: (error?: unknown) => void
-  
+
   const promise = new Promise<T>((resolve, reject) => {
     resolvePromise = resolve
     rejectPromise = reject
@@ -29,42 +26,36 @@ function mkDeferred<T>(): Deferred<T> {
   }
 }
 
-export
-function genRandom(size: number): Buffer {
+export function genRandom(size: number): Buffer {
   return crypto.randomBytes(size)
 }
 
-export
-function genRandomString(size: number): string {
+export function genRandomString(size: number): string {
   return genRandom(size).toString('hex')
 }
 
-export
-function sha256(value: Buffer): Buffer {
+export function sha256(value: Buffer): Buffer {
   return crypto.createHash('sha256').update(value).digest()
 }
 
-export
-function encodeBase64(value: Buffer): string {
+export function encodeBase64(value: Buffer): string {
   return value
     .toString('base64')
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
-    .replace(/=/g, '');
+    .replace(/=/g, '')
 }
 
 
-export
-type Timeout<T> = {
+export interface Timeout<T> {
   cancel: () => void
   promise: Promise<T>
 }
 /**
  * Returns a promise that ALWAYS REJECTS after `duration`.
  */
-export
-function startTimeout<T = unknown>(duration: number): Timeout<T> {
-  let cancel: () => void 
+export function startTimeout<T = unknown>(duration: number): Timeout<T> {
+  let cancel: () => void
 
   const promise = new Promise<T>((_, reject) => {
     const timeout = setTimeout(reject, duration)
